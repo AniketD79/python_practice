3fdd67b5e8fbc3d0246f5e1ef898163d7f2cb3e7
Aniket Darje <aniketdarje9@gmail.com>
2025-04-22 22:53:17 +0530

    hi

--- first.py
+++ first.py
@@ -10,4 +10,18 @@ print(len(str2))
 str3= str1 + "  "+ str2
 print(str3)
 print(str3[5]) # Prints character at 5
+
+        #Slicing
 print(str3[2:5]) # prints characters from index 2 to 4
+
+name= "ANIKET"
+#This is normal and forward counting
+#0 1 2 3 4 5
+#A N I K E T
+print(name[0:4]) #ANIK
+print(name[0: len(name)]) #ANIKET
+
+#Backward counting that is starting from last
+# -6 -5 -4 -3 -2 -1
+# A   N  I  K  E  T
+print(name[-1 : -4 : -1]) #TEK
