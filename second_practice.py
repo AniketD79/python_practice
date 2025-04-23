5afc5db445c2e2021221a23d2287acabea988263
Aniket Darje <aniketdarje9@gmail.com>
2025-04-23 22:33:28 +0530

    hi

---
+++ second_practice.py
@@ -0,0 +1,33 @@
+# Q1
+# a=input("Enter your first favourite movie name:")
+# b=input("Enter your second favourite movie name:")
+# c=input("Enter your third favourite movie name:")
+
+# d=[]
+# d.append(a)
+# d.append(b)
+# d.append(c)
+# print(d)
+
+# Q2 If list contains palindrome
+
+# a=[1,2,3,2,1,4]
+# b=a.copy()
+# b.reverse()
+
+# print(b)
+# if(a==b):
+#     print("List is a palindrome")
+# else:
+#     print("List is not a palindrome")
+
+# Q3 Count A in tuple
+
+tuple1=("A", "B", "C", "D", "B","C", "A", "D", "C", "A")
+print(tuple1.count("A"))
+
+
+# Q4 sort list A to Z
+a=["A", "B", "C", "D", "B","C", "A", "D", "C", "A"]
+a.sort()
+print(a)
