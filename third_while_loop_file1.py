2da5bec9aabfc0ccc456429bcd2b44626a2922da
Aniket Darje <aniketdarje9@gmail.com>
2025-04-24 23:17:25 +0530

    hi

---
+++ third_while_loop_file1.py
@@ -0,0 +1,11 @@
+#While loop
+
+#while condition:
+ #   statement 
+#Keywords
+#Break: Used to terminate the loop when encounters break
+#Continue: Terminates execution in the current iteration and continues execution of the loop with next iteration
+#
+#
+#
+#
--- third_while_loop_practice1.py
+++ third_while_loop_practice1.py
@@ -1,2 +1,37 @@
 
-#Q1
+# #Q1
+# num=1
+# while num<=100:
+#     print(num)
+#     num+=1
+
+# #Q2
+# num=100
+# while num>=1:
+#     print(num)
+#     num-=1
+
+# #Q3
+# num=1
+# while num<=10:
+#     print(num*2)
+#     num+=1
+
+# #Q4
+# list1=[1,4,9,16,25,36,49,64,81,100]
+
+# num=0
+# while num<len(list1):
+#     print(list1[num])
+#     num+=1
+
+#Q5
+# tuple1=(1,4,9,16,25,36,49,64,81,100)
+# x=36
+# num=0
+# while num<len(tuple1):
+#     if(tuple1[num]==x):
+#         print(num, tuple1[num])
+#     num=num+1
+
+    
