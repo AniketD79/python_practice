6100822bee68804809eb41923b35965144d69491
Aniket Darje <aniketdarje9@gmail.com>
2025-04-24 23:25:44 +0530

    hi

--- third_while_loop_file1.py
+++ third_while_loop_file1.py
@@ -8,4 +8,19 @@
 #
 #
 #
-#
+#Below will print 1 to 4 and break when 5
+# num=1
+# while num<=10:
+#     if(num==5):
+#         break
+#     print(num)
+#     num+=1
+
+#Below will skip 4 and print rest all
+num=1
+while num<=10:
+    if(num==4):
+        num+=1
+        continue
+    print(num)
+    num+=1
