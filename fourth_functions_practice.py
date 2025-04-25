3058b323ca7ef7db22bbf0bf5c617c491f7cac49
Aniket from Office <aniketdarje9@gmail.com>
2025-04-25 19:09:23 +0530

    commit

--- fourth_functions_practice.py
+++ fourth_functions_practice.py
@@ -23,11 +23,11 @@
 # printElements([1,2,6,3,4,5,6,7])
 
 #Some question
-def even_odd(a):
-    a=int(input("Enter a num"))
-    if(a%2==0):
-        print("Even")
-    else:
-        print("Odd")
+# def even_odd(a):
+#     a=int(input("Enter a num"))
+#     if(a%2==0):
+#         print("Even")
+#     else:
+#         print("Odd")
 
-even_odd(input)
+# even_odd(input)
