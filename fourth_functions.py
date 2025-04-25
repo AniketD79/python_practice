4eaee3b782acb3cfc678d7a415f024c4fc2df77f
Aniket from Office <aniketdarje9@gmail.com>
2025-04-25 18:36:35 +0530

    functions

---
+++ fourth_functions.py
@@ -0,0 +1,18 @@
+#Functions:
+#Block of statements that perform a specific task.
+
+#def func_name9param1, param2..): 
+#Some work
+#return  val
+
+# def add(a,b):
+#     return a+b
+
+# print(add(4,5))
+
+# If a function doesn't returns anything then it returns None
+
+def average(a,b,c):
+    return (a+b+c)/3
+
+print(average(1,2,3))
---
+++ fourth_functions_practice.py
@@ -0,0 +1,33 @@
+
+
+# Q1
+
+# def printLength(a=[]):
+#     print(len(a))
+
+# printLength([1,2,3,4,5])
+
+# Q1 second way
+
+# def printLength(list):
+#     print(len(list))
+
+# printLength([1,2,3,4])
+
+
+# Q2
+# def printElements(a=[]):
+#     for i in a:
+#         print(i, end="")
+
+# printElements([1,2,6,3,4,5,6,7])
+
+#Some question
+def even_odd(a):
+    a=int(input("Enter a num"))
+    if(a%2==0):
+        print("Even")
+    else:
+        print("Odd")
+
+even_odd(input)
