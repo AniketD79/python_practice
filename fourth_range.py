f4205623ecadd9502d7fc9d8cfb4f3232230e6cb
Aniket from Office <aniketdarje9@gmail.com>
2025-04-25 17:52:24 +0530

    range

--- fourth_range.py
+++ fourth_range.py
@@ -1,8 +1,14 @@
 #Range
 # range() : Range functions returns a sequence of numbers starting from 0
 # by default & increments by 1(by default), and stops before a specified number
-#
-print(range(5)) # o/p= range(0,5)
+#range(start?, stop, step?)
+# print(range(5)) # o/p= range(0,5)
 
-seq= range(6)
-print(seq[5])
+# seq= range(6)
+# print(seq[5])
+
+#Printing table using range
+num=3
+for i in range(1, 11):
+        print(num*i)
+        # i+=1
