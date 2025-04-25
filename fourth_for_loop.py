02eb1e9e65c82b2c3c78f8f6e3fa101b7856acd6
Aniket from Office <aniketdarje9@gmail.com>
2025-04-25 16:42:27 +0530

    for loops

---
+++ fourth_for_loop.py
@@ -0,0 +1,13 @@
+# list1=[1,2,3,4,5,6,]      
+
+# for num in list1:
+#     print(num)
+
+tuple1=(1,2,3,4,5,6,7,8,9,)
+for i in tuple1:
+    if(i==8):
+        print(i)
+        break
+    # print(i)
+else:
+    print("Ended")
